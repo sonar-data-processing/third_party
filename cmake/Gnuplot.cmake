5b44233cfd3a9fb8b199329f6d3aabfe9242958d
Gustavo Neves <gustavo.neves12@gmail.com>
2017-07-05 16:32:44 -0300

    adapt to autoproj

--- cmake/Gnuplot.cmake
+++ cmake/Gnuplot.cmake
@@ -1,14 +1,7 @@
 if (NOT DEFINED gnuplot_ROOT_DIR)
     set (
-        gnuplot_ROOT_DIR
-        "${WORKSPACE_DIR}/third_party/gnuplot-iostream"
-        CACHE PATH "Gnuplot Root Directory"
-        FORCE
-    )
-
-    set (
         gnuplot_INCLUDE_DIR
-        "${WORKSPACE_DIR}/third_party/gnuplot-iostream"
+        "${CMAKE_INSTALL_PREFIX}/include/sonar_toolkit/third_party/gnuplot-iostream"
         CACHE PATH "Gnuplot Include Directory"
         FORCE
     )
